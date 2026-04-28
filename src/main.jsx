7c00849f745c72b322c9ff173c1824baa0d3332b
aaryan218 <aaryanbarthwal2187@gmail.com>
2026-04-28 10:52:31 +0530

    feat: setup global UI system

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/theme.css";
+import "./styles/spacing.css";
+import "./styles/typography.css";
+import "./styles/global.css";
+import "./styles/card.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
